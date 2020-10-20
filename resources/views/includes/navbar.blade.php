5bb109735548637b8cd23c79dd0648beba48d4c7
abdullahmohsen <44713320+abdullahmohsen@users.noreply.github.com>
2020-10-20 11:56:29 +0200

    first comit

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,21 @@
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+  <a class="navbar-brand" href="home">BP Website</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExample04">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="home">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="services">Services</a>
+      </li>
+    </ul>
+  </div>
+</nav>
